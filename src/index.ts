ce7431d22788c3b0c02984a0f7d09e724d8a9493
Michael Nadeau <waaverecords@gmail.com>
2021-11-05 19:04:27 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const port = 3000;
 const app = express();
 
 app.get('*', (request, response) => {
-    response.send({ message: 'Hello World!'});
+    response.send({ message: 'Hello World!!'});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
