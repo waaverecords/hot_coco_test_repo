6a37fbb4b4561a0672f5fe4ce725b5b3d7613d83
Michael Nadeau <waaverecords@gmail.com>
2022-01-14 06:42:47 -0500

    change

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import express from 'express';
 
 const port = 3000;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.get('*', (request, response) => {
     response.send({ message: 'Hello World!!!!!!!! test change'});
 });
+
+app.post('*', (request, response) => {
+    console.log(request.body);
+    response.status(200).send();
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
